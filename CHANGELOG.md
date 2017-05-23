89b025d68fe16ff9733b6575a7773ac2be874ca0
ansumanshah <ansumanshah@gmail.com>
2017-05-23 15:29:59 +0530

    init

---
+++ CHANGELOG.md
@@ -0,0 +1,2 @@
+## 0.1.0 - First Release
+* Can convert CSS to JS inline styles and vice versa
---
+++ spec/css-in-js-spec.js
@@ -0,0 +1,17 @@
+'use babel';
+
+import CssInJs from '../lib/css-in-js';
+
+// Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
+//
+// To run a specific `it` or `describe` block add an `f` to the front (e.g. `fit`
+// or `fdescribe`). Remove the `f` to unfocus the block.
+
+describe('CssInJs', () => {
+  let workspaceElement, activationPromise;
+
+  beforeEach(() => {
+    workspaceElement = atom.views.getView(atom.workspace);
+    activationPromise = atom.packages.activatePackage('css-in-js');
+  });
+});
