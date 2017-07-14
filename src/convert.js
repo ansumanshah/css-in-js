c22f7c47a7cb99fc296dbf888ff18648692fdaa6
Paul Molluzzo <paul@molluzzo.com>
2017-07-14 11:34:00 -0400

    Refactor convert and fix beginning whitespace issue

--- src/convert.js
+++ src/convert.js
@@ -5,57 +5,42 @@
 
 'use babel'
 
-let isCSS = item => {
+const getBeginningWhitespace = string =>
+  string.match(/^\s+/) !== null ? string.match(/^\s+/)[0] : ''
+
+const isCSS = item => {
   return (item.match(/;/g) || []).length === 1
 }
 
-let toCamel = prop => {
-  return prop.replace(/-(\w|$)/g, (dash, next) => next.toUpperCase())
-}
-
-let toJS = item => {
-  let [prop, val] = item.split(':')
-  let whiteSpace = prop.indexOf(prop.trim()) + 1
-  return (
-    Array(whiteSpace).join(' ') +
-    toCamel(prop.trim()) +
-    ": '" +
-    val.trim().replace(';', '') +
-    "',"
-  )
-}
-
-let isJS = item => {
+const isJS = item => {
   return (item.match(/,/g) || []).length === 1
 }
 
-let toHyphen = prop => {
+const toHyphen = prop => {
   return prop.replace(/([A-Z])/g, char => `-${char[0].toLowerCase()}`)
 }
 
-let toCSS = item => {
-  let [prop, val] = item.split(':')
-  let whiteSpace = prop.indexOf(prop.trim()) + 1
-  return (
-    Array(whiteSpace).join(' ') +
-    toHyphen(prop.trim()) +
-    ': ' +
-    val.trim().replace(/'|,/g, '') +
-    ';'
-  )
+const toCamel = prop => {
+  return prop.replace(/-(\w|$)/g, (dash, next) => next.toUpperCase())
+}
+
+const toJS = item => {
+  const [prop, val] = item.split(':')
+  return `${getBeginningWhitespace(prop)}${toCamel(
+    prop.trim()
+  )}: '${val.trim().replace(';', '')}',`
+}
+
+const toCSS = item => {
+  const [prop, val] = item.split(':')
+  return `${getBeginningWhitespace(prop)}${toHyphen(
+    prop.trim()
+  )}: ${val.trim().replace(/'|,/g, '')};`
 }
 
 export default function convert (s) {
-  let lines = s.split('\n')
+  const lines = s.split('\n')
   return lines
-    .map(item => {
-      if (isCSS(item)) {
-        return toJS(item)
-      } else if (isJS(item)) {
-        return toCSS(item)
-      } else {
-        return item
-      }
-    })
+    .map(item => (isCSS(item) ? toJS(item) : isJS(item) ? toCSS(item) : item))
     .join('\n')
 }
